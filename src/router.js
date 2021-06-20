cb921fdc4835fb21bca0bdf2d7d2ad99d0627e6c
minagawad <minagerges651@gmail.com>
2021-06-21 00:15:34 +0200

    first commit

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachRegister from './pages/coaches/CoachRegister.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import { NotFound } from './pages/NotFound.vue';
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', redirect: '/coaches' },
+      { path: '/coaches', component: CoachList },
+      {
+        path: '/coaches/:id',
+        component: CoachDetails,
+        children: [
+          { path: 'contact', component: ContactCoach } // /coaches/c1/contact
+        ]
+      },
+      { path: '/register', component: CoachRegister },
+      { path: '/requests', component: RequestReceived },
+      { path: '/:notFound(.*)', component: NotFound }
+    ]
+  });
+  
+  export default router;
