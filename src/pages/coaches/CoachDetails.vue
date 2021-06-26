32360025a0bf89b6318c0454de659682ea4947ae
minagawad <minagerges651@gmail.com>
2021-06-26 02:22:40 +0200

    Adding  UI and coach Details And Coach Filter

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,9 +1,78 @@
 <template>
 
-   Details of coach
-   <router-view ></router-view>
-<router-link to="/coaches/c1/contact">deatails </router-link>
+  <section>
+<base-card>
+<h2>{{fullName}}</h2>
+<h3>${{rate}} /hour</h3>
+</base-card>
+
+  </section>
+  <section>
+
+     <base-card>
+     <header>
+        <h2>Interested ? Reach Out Now!</h2>
+        <base-button link :to="contactLink">contact</base-button>
+     </header>
+     <router-view></router-view>
+     </base-card>
+  </section>
+
+  <section>
+
+     <base-card>
+     <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+     <p>{{description}}</p>
+     </base-card>
+  </section>
+
+
 
     
 
 </template>
+
+><script>
+export default {
+   props:['id'],
+
+   data()
+   {
+      return {selectedCoach:null};
+
+   },
+   computed:
+   {
+      fullName()
+      {
+         return this.selectedCoach.firstName +' ' +this.selectedCoach.lastName;
+      },
+      rate()
+      {
+         return this.selectedCoach.hourlyRate;
+      }
+      ,
+      description()
+      {
+return this.selectedCoach.description;
+
+      },
+      areas()
+      {
+return this.selectedCoach.areas;
+      },
+
+      contactLink()
+      {
+         return this.$route.path +'/' +this.id+'/contact';
+      }
+   }
+,
+   created()
+   {
+    this.selectedCoach=  this.$store.getters['coaches/coaches'].
+    find((coach)=>coach.id===this.id);
+
+   }
+}
+</script>
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,28 +1,74 @@
 <template>
-  <section>Filter</section>
   <section>
+<coach-filter @change-filter="setFilters"></coach-filter>
+    
+  </section>
+  <section>
+
+    <base-card>
     <div class="controls">
 
-      <button>Refresh</button>
-      <router-link to="/register"> Register as coach</router-link>
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register"> Register as coach</base-button>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filterCoaches" :key="coach.id">
-        {{coach.firstName}}
-      </li>
+      <coach-item v-for="coach in filterCoaches" :key="coach.id" 
+      :id="coach.id" 
+      :firstName="coach.firstName" 
+      :lastName="coach.lastName" 
+      :rate="coach.hourlyRate"
+      :areas="coach.areas" >
+      </coach-item>
     </ul>
 <h3 v-else> there is no coaches </h3>
 
     List of Coaches
+    </base-card>
   </section>
 </template>
 
 <script>
+
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 export default {
+  components:{
+    CoachItem,
+    CoachFilter
+  },
+  data()
+  {
+    return {
+      activeFilter:{
+        frontend:true,
+        backend:true,
+        career:true
+
+      }
+    }
+  },
  computed:{
 filterCoaches()
 {
-  return this.$store.getters['coaches/coaches'];
+  const coach= this.$store.getters['coaches/coaches'];
+  return coach.filter(coach=>{
+if(this.activeFilter.frontend&&coach.areas.includes('frontend')
+)
+{
+  return true;
+}
+if(this.activeFilter.backend&&coach.areas.includes('backend')
+)
+{
+  return true;
+}
+if(this.activeFilter.career&&coach.areas.includes('career')
+)
+{
+  return true;
+}
+return false;
+  });
 },
 hasCoaches()
 {
@@ -30,5 +76,25 @@ hasCoaches()
 }
 
  },
+ methods:{
+   setFilters(updatedFilter)
+   {
+     this.activeFilter=updatedFilter;
+
+   }
+ }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
