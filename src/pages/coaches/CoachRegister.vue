83fc2db416643f454506fd280b49e0069be2f564
minagawad <minagerges651@gmail.com>
2021-06-28 00:10:34 +0200

    adding contact coach and validations

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,29 @@
 <template>
-    Register
+    <section>
+<base-card>
+        <h2>
+            Register as a coach now!
+        </h2>
+        <coach-form @save-data="saveData"></coach-form>
+        </base-card>
+    </section>
+
+
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+    components:{
+        CoachForm,
+    },
+    methods:{
+        saveData(data)
+        {
+            this.$store.dispatch('coaches/registerCoach',data);
+            this.$router.push('/coaches');
+        }
+    }
+    
+}
+</script>
